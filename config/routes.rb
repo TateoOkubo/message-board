88d188d2468796fa1c0f4813fb44e501c680d9e3
Tateo Okubo <neteooo@gmail.com>
2016-12-17 16:35:05 +0000

    using Bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   #get 'messages/index'
   root 'messages#index'
   
+  #add
+  resources :messages , only: [:create]
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
